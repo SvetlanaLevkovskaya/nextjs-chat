d5baa4f26d83629d55a22edbf970e104060d7eb3
svetlana <levkovskayase@gmail.com>
2024-07-09 21:34:28 +0300

    feat: fix styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,22 +7,24 @@ import styles from './header.module.scss'
 
 export const Header = () => (
   <header className={styles.wrapper}>
-    <div className={styles.imageWrapper}>
-      <Image src="/avatar-group.png" alt="avatar-group" width={100} height={50} priority />
-    </div>
-
-    <div className={styles.logoWrapper}>
-      <div className={styles.subWrapper}>
-        <CrownTwoTone twoToneColor="#eb2f96" />
-
-        <h4>Team Unicorns</h4>
+    <div className={styles.header}>
+      <div className={styles.imageWrapper}>
+        <Image src="/avatar-group.png" alt="avatar-group" width={100} height={50} priority />
       </div>
 
-      <span>last seen 45 minutes ago</span>
-    </div>
+      <div className={styles.logoWrapper}>
+        <div className={styles.subWrapper}>
+          <CrownTwoTone twoToneColor="#eb2f96" />
 
-    <div className={styles.iconWrapper}>
-      <EllipsisOutlined style={{ color: '#666668' }} />
+          <h4>Team Unicorns</h4>
+        </div>
+
+        <span>last seen 45 minutes ago</span>
+      </div>
+
+      <div className={styles.iconWrapper}>
+        <EllipsisOutlined style={{ color: '#666668' }} />
+      </div>
     </div>
   </header>
 )
